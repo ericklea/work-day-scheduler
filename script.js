e8ff197c31f62c600c930529658959fefaa13558
L E <erickson.leah.j@gmail.com>
2023-03-27 19:50:33 -0600

    updated javascript

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+ 
 
-
-
-  //local storage
+   //local storage
   var storage = JSON.parse(localStorage.getItem('saveCal')) || []
 
   var currentHour = dayjs().format('HH');
@@ -32,12 +21,8 @@ $(document).ready(function () {
   })
 
   localStorage.setItem('saveCal', JSON.stringify (storage));
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-
-
   
+
   //displays current day
   var day = dayjs().format('MMM DD, YYYY');
   $('#currentDay').text(day);
@@ -52,7 +37,7 @@ $(document).ready(function () {
   $('#hour-17 .description').val(localStorage.getItem('hour-17'))
 
 
-  //function to set color to coencide with the time
+  //function to set color to coincide with the time
   function colorChange() {
     $('.time-block').each(function () {
 
